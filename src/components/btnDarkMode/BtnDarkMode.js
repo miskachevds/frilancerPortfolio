ee46f7e11360923f6336bdb9307c38b42b97c285
Miskachev Dima <miskachevds@yandex.ru>
2026-02-04 16:18:29 +0300

    sistem dark mode+

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,35 +1,39 @@
 
-import { useState, useEffect } from "react";
+import { useEffect, useRef } from "react";
+import {useLocalStorage} from './../../utils/useLocalStorage';
+import detectDarkMode from "../../utils/detectDarkMode";
+
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
 import './style.css'
 
+
 const BtnDarkMode = () => {
+    // const [darkMode, setDarkMode] = useState('light')//вызывая setDarkMode меняем состояние
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', detectDarkMode() )//вместо useState используем useLocalStorage
 
-    const [darkMode, setDarkMode] = useState('light')//вызывая setDarkMode меняем состояние
+    const btnRef = useRef(null);
 
-    useEffect(()=>{
-        // console.log('Use effect fired');
-        // console.log(darkMode);
+    useEffect(()=>{//выполняется один раз
 
         if(darkMode === 'dark'){
             document.body.classList.add('dark')
+            btnRef.current.classList.add('dark-mode-btn--active')
         }else{
             document.body.classList.remove('dark');
+            btnRef.current.classList.remove('dark-mode-btn--active')
         }
-
     }, [darkMode]);
 
     const toggleDarcMode = () =>{//нажали=вызвали
         setDarkMode((currentValue)=>{//здесь меняем сост на состояние
             return currentValue === 'light' ? 'dark': 'light';
         })
-
-        // console.log('toggleDarcMode')
+    
     }
 
-    return (
-        <button className="dark-mode-btn" onClick={toggleDarcMode}>
+    return (//при клике toggleDarcMode
+        <button ref={btnRef} className="dark-mode-btn" onClick={toggleDarcMode}>
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
             <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
         </button>
---
+++ src/utils/detectDarkMode.js
@@ -0,0 +1,11 @@
+
+
+export default detectDarkMode = () => {
+    // 1. Проверка темной темы на уровне системных настроек
+    if (window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        return 'dark'
+    }
+    return 'light'
+}
